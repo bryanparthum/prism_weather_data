80d3d82818a845351c7928cfdf74188b7b1a2679
Bryan Parthum <parthum.bryan@epa.gov>
2022-09-03 07:19:55 -0400

    include nhd api download

--- code/get_prism_parallel.r
+++ code/get_prism_parallel.r
@@ -34,50 +34,45 @@ lapply(list.of.packages, library, character.only = TRUE)
 ###################  parts
 ##########################
 
-# # # ## get shape from nhd
-# # # tf  = tempfile()
-# # # url = 'https://edap-ow-data-commons.s3.amazonaws.com/NHDPlusV21/Data/NationalData/NHDPlusV21_NationalData_WBDSnapshot_Shapefile_08.7z'
-# # # download.file(url , tf)
-# # # archive_extract(tf)
-# # 
-# # ## import raw nhd data
-# # polys =
-# #   st_read('NHDPlusNationalData/WBDSnapshot_National.shp') %>%
-# #   rename_all(., .funs = tolower)
-# # 
-# # # get usa map (conus only)
-# # usa =
-# #   get_urbn_map("states", sf = TRUE) %>%
-# #   filter(!(state_abbv %in% c('AK','HI'))) %>%
-# #   st_union
-# # 
-# # ## trim huc12 to conus
-# # sf::sf_use_s2(FALSE) ## this is an issue with a particular version of sf. fix later
-# # ## this seems to filter out 2,000 too many, maybe st_within is "entirely within"
-# # # polys %<>%
-# # #   dplyr::select(huc_12, geometry) %>%
-# # #   st_filter(usa, .predicate = st_within)
-# # tic()
-# # polys = st_transform(polys, st_crs(usa))[usa,]
-# # toc()
-# # 
-# # ## export to skip computation time above
-# # polys %>% st_write('store/huc12_conus/huc12_conus.shp')
-# 
-# ## open a prism raster and reproject so that the huc12s are already in the correct projection
-# options(prism.path = "store/temporary")
-# get_prism_dailys(type = "ppt", minDate = "1999-12-31", maxDate = "1999-12-31", keepZip = F)
-# dir    = "store/temporary"
-# subdir = list.files("store/temporary")
-# path   = list.files("store/temporary", full.names = T)
-# raster = raster(paste(path, "/", subdir, ".bil", sep = ""))
-# polys =
-#   st_read('store/huc12_conus/huc12_conus.shp') %>%
-#   st_transform(st_crs(raster))
-# # ## check projections
-# polys %>% st_write('store/huc12_conus_with_prism_crs/huc12_conus_with_prism_crs.shp')
-# plot(polys)
-# plot(raster, add = T)
+## get shape from nhd
+tf  = tempfile()
+url = 'https://edap-ow-data-commons.s3.amazonaws.com/NHDPlusV21/Data/NationalData/NHDPlusV21_NationalData_WBDSnapshot_Shapefile_08.7z'
+download.file(url , tf)
+archive_extract(tf)
+
+## import raw nhd data
+polys =
+  st_read('NHDPlusNationalData/WBDSnapshot_National.shp') %>%
+  rename_all(., .funs = tolower)
+
+## get usa map (conus only)
+usa =
+  get_urbn_map("states", sf = TRUE) %>%
+  filter(!(state_abbv %in% c('AK','HI'))) %>%
+  st_union
+
+## trim huc12 to conus
+sf::sf_use_s2(FALSE) ## this is an issue with a particular version of sf. fix later
+polys = st_transform(polys, st_crs(usa))[usa,]
+
+## export to skip computation time above
+polys %>% st_write('store/huc12_conus/huc12_conus.shp')
+
+## open a prism raster and reproject so that the huc12s are already in the correct projection
+options(prism.path = "store/temporary")
+get_prism_dailys(type = "ppt", minDate = "1999-12-31", maxDate = "1999-12-31", keepZip = F)
+dir    = "store/temporary"
+subdir = list.files("store/temporary")
+path   = list.files("store/temporary", full.names = T)
+raster = raster(paste(path, "/", subdir, ".bil", sep = ""))
+polys =
+  st_read('store/huc12_conus/huc12_conus.shp') %>%
+  st_transform(st_crs(raster))
+
+## check projections
+polys %>% st_write('store/huc12_conus_with_prism_crs/huc12_conus_with_prism_crs.shp')
+plot(polys)
+plot(raster, add = T)
 
 ## read from above
 polys = 
@@ -89,9 +84,8 @@ polys =
 ##########################
 
 ## parallel filter and writing of feather files
-# parallel::detectCores()
-# n.cores <- parallel::detectCores() - 1
-n.cores = 20
+parallel::detectCores()
+n.cores <- parallel::detectCores() - 1
 
 ## make cluster
 my.cluster <- parallel::makeCluster(
@@ -161,4 +155,4 @@ foreach(DATE      = as.character(seq(ymd("2000-01-01"), ymd("2022-07-31"), "days
 ## stop cluster
 parallel::stopCluster(cl = my.cluster)
 
-## End of script, have a nice day!
+## end of script, have a nice day!
